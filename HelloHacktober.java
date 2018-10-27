73da3c7020ec4cfe052e9c153f7d87531ecf9374
Lesterdg <44044368+Lesterdg@users.noreply.github.com>
2018-10-27 22:53:30 +0800

    Add 2018

--- HelloHacktober.java
+++ HelloHacktober.java
@@ -2,6 +2,6 @@ public class HelloHacktober {
     
     public static void main(String[] args) {
         
-        System.out.println("Hello Hacktober!");
+        System.out.println("Hello Hacktober 2018!");
     }
 }
